d21a5d08b0fc69743070b1bd0518489181f110fb
Roberto Biello <r.s.biello@gmail.com>
2025-12-15 11:28:33 +0100

    Add ADMIXTURE script for running analyses

---
+++ admixture/admixture.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# ADMIXTURE
+source /path/to/software/admixture-1.3.0
+
+# Parameters
+OUT=example_dataset.filtered
+BED=./input/example_dataset.filtered.bed
+THREADS=16
+REPS=20
+
+# Run ADMIXTURE for K = 1 to 10, with 20 replicates each
+for K in {1..10}; do
+  for replicate in $(seq 1 "$REPS"); do
+    admixture -s time --cv=10 "$BED" "$K" -j"$THREADS" \
+      | tee "log_K${K}_rep${replicate}.out"
+
+    mv "${OUT}.${K}.Q" "${OUT}.K${K}_rep${replicate}.Q"
+    mv "${OUT}.${K}.P" "${OUT}.K${K}_rep${replicate}.P"
+  done
+done
